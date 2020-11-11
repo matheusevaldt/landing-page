1adc42acbb93481e797c0d1f96fdd6fad2315825
Matheus Evaldt <evaldtmatheus@gmail.com>
2020-11-11 18:55:14 -0300

    Working on the mobile menu and the landing page info

--- src/script.js
+++ src/script.js
@@ -1,4 +1,18 @@
 const header = document.getElementsByTagName('header')[0];
+const mobileMenu = document.querySelector('.mobile-menu');
+const buttonOpenMobileMenu = document.querySelector('.button-open-mobile-menu');
+const buttonCloseMobileMenu = document.querySelector('.button-close-mobile-menu');
+
+buttonOpenMobileMenu.addEventListener('click', openMobileMenu);
+buttonCloseMobileMenu.addEventListener('click', closeMobileMenu);
+
+function openMobileMenu() {
+    mobileMenu.classList.add('open-mobile-menu');
+}
+
+function closeMobileMenu() {
+    mobileMenu.classList.remove('open-mobile-menu');
+}
 
 window.addEventListener('scroll', () => {
     const amountScrolled = window.scrollY;
