e1772871fa738f9391b75f139bc3db972c526e35
Matheus Evaldt <evaldtmatheus@gmail.com>
2020-11-12 23:45:25 -0300

    Improving the animation effects in the mobile menu

--- src/script.js
+++ src/script.js
@@ -3,23 +3,52 @@ const mobileMenu = document.querySelector('.mobile-menu');
 const buttonOpenMobileMenu = document.querySelector('.button-open-mobile-menu');
 const buttonCloseMobileMenu = document.querySelector('.button-close-mobile-menu');
 const mobileMenuItems = document.querySelector('.mobile-menu-items');
-
-const landingPage = document.querySelector('.landing-page');
+const mobileMenuOuter = document.querySelector('.mobile-menu-outer');
 
 buttonOpenMobileMenu.addEventListener('click', openMobileMenu);
 buttonCloseMobileMenu.addEventListener('click', closeMobileMenu);
+mobileMenuOuter.addEventListener('click', closeMobileMenu);
 
 function openMobileMenu() {
     mobileMenu.classList.add('open-mobile-menu');
-    landingPage.classList.add('blur-landing-page');
-    setTimeout(() => mobileMenuItems.classList.add('display-mobile-menu-items'), 600);
+    displayMobileMenuItems();
 }
 
 function closeMobileMenu() {
     mobileMenu.classList.remove('open-mobile-menu');
-    landingPage.classList.remove('blur-landing-page');
-    mobileMenuItems.classList.remove('display-mobile-menu-items');
+    clearMobileMenu();
 }
+
+function displayMobileMenuItems() {
+    const amountOfItems = mobileMenuItems.childElementCount;
+    let itemsDisplayed = 0;
+    for (let i = 0; i < amountOfItems; i++) {
+        setTimeout(() => {
+            mobileMenuItems.children[i].classList.add('display-mobile-menu-items');
+            itemsDisplayed++;
+            if (amountOfItems === itemsDisplayed) {
+                setTimeout(() => {
+                    buttonCloseMobileMenu.classList.add('display-button-close-mobile-menu');
+                }, 500);
+            }
+        }, i * 200);
+    }
+}
+
+function clearMobileMenu() {
+    const amountOfItems = mobileMenuItems.childElementCount;
+    for (let i = 0; i < amountOfItems; i++) {
+        mobileMenuItems.children[i].classList.remove('display-mobile-menu-items');
+    }
+    buttonCloseMobileMenu.classList.remove('display-button-close-mobile-menu');
+}
+
+
+
+
+
+
+
 
 window.addEventListener('scroll', () => {
     const amountScrolled = window.scrollY;
